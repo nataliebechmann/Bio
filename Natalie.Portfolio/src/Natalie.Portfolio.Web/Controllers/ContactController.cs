02015ecb1b0536f8f384a394435503c0ed25644a
Natalie Bechmann <natalie.bechmann@gmail.com>
2017-01-02 21:19:00 +0000

    added contact controller

---
+++ Natalie.Portfolio/src/Natalie.Portfolio.Web/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Natalie.Portfolio.Web.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Natalie.Portfolio.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(ContactViewModel contactViewModel)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Natalie.Portfolio/src/Natalie.Portfolio.Web/Models/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Natalie.Portfolio.Web.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
